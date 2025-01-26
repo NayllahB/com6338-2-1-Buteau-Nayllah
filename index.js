65346daf16a443017effb7070e6e010bfad21acd
Nayllah Buteau <nayllah.buteau@ufl.edu>
2025-01-25 22:18:16 -0500

    uploading solved code

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+
+function greet() {
+    var userName = prompt('What is your name?')
+    alert('Hello, ' + userName)
+    var ageStr = prompt('How old are you?')
+    var userAge = parseInt(ageStr)
+    if (!userAge) return
+    var birthdayYet = confirm('Have you had a birthday yet this year?')
+    var currentYear = new Date().getFullYear()
+    if (birthdayYet===true) {
+        var userYearBorn = currentYear - userAge
+        alert('You were born in ' + userYearBorn)
+    } else {
+        userYearBorn = currentYear - (userAge + 1)
+        alert('You were born in ' + userYearBorn)
+    }
+    console.log (userYearBorn)
+}
